181f39485e5f145ab6cdb480f22a552e8663c14e
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-04-25 16:26:06 +0330

    first Axios

---
+++ src/ConcurrencyAxios.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import React,{useEffect} from "react";
+
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+const ConcurrencyAxios = () => {
+    useEffect(() =>{
+        getAll();
+
+    }, []);
+
+    const getAll = () => {
+        Promise.all([
+            axios.get(`${BASE_URL}/Posts`),
+            axios.get(`${BASE_URL}/Comments`),
+        ])
+        .then(
+            axios.spread((posts,comments) => {
+                console.log(posts.data,comments.data)
+            })
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+    return <div></div>
+}
+export default ConcurrencyAxios;
